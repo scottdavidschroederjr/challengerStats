642af4e9f42c3dbcb5dc0e827ba59c7ff7d6b163
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-06-07 18:33:51 -0400

    Update script.js
    This code can compare if two players have duo'd in the last 100 matches. Next steps is to request at least 1000 matches and work my way around the rate limiting.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //these will be the inputs on the site to pull the info
-var apiKey = ""
+var apiKey = "RGAPI-9144b35c-bceb-4dd3-aa47-5ab89fc33464"
 var userName1 = "SaveAsUntitled"
 var userName2 = "plsperish"
 var setCoreName = "TFTSet7_2"
@@ -23,39 +23,56 @@ var output = {
   }
 }
 
-console.log(output)
-
-//putting pthe URLs needed for each request together
-
-
-async function fetchData(requestInput, typeOfRequest = false) {
+async function fetchData(requestInput, typeOfRequest = false, username) {
 
   //for converting user's username into PUUID
     if (typeOfRequest === "puuid") {
-      var requestURL = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/" + requestInput + "?api_key=" + apiKey;
-      const response = await fetch(requestURL)
-      const data = await response.json();
-      console.log(output)
+      
+      let requestURL = "https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/" + requestInput + "?api_key=" + apiKey;
+      let response = await fetch(requestURL)
+      let data = await response.json();
       output[requestInput]["puuid"] = data["puuid"]
-      return
+      return output[requestInput]["puuid"]
     }
 
   //for grabbing list of matches
+    else if (typeOfRequest === "matchList"){
+      var sectionOfMatches = 0
 
+      var matchRequestURL = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + requestInput + "/ids?start=" + sectionOfMatches + "&count=100&api_key=" + apiKey;
+      
+
+      var response = await fetch(matchRequestURL)
+      var data = await response.json();
+
+      
+      output[username]["matches"] = data
+
+      //starts comparison once matches have been pulled for both users
+      if (Object.keys(output[userName1]["matches"]).length >= 100 && Object.keys(output[userName2]["matches"]).length >= 100) {
+        const intersection = output[userName1]["matches"].filter(element => output[userName2]["matches"].includes(element));
+        console.log(output[userName1]["puuid"])
+        console.log(output[userName2]["puuid"])
+        console.log(intersection)
+      }
+  
+
+      }
   }
 
 
 
-fetchData(userName1, 'puuid').then(
-fetchData(userName2, "puuid")).then()
-//request U1 match list
-//request U2 match list
+
+
+fetchData(userName1, "puuid").then(
+user1PUUID => fetchData(user1PUUID, "matchList", userName1))
+
+fetchData(userName2, "puuid").then(
+user2PUUID => fetchData(user2PUUID, "matchList", userName2))
+
 //function that cuts down match list to only matches that both users are in
 //request duo match data for U1
 //request duo match data for U2
-
-
-
 
 
 
