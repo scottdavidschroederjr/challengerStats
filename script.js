854b0d9bd73ad948999972a127c97891c94539c3
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-06-08 17:40:57 -0400

    TO DO: make functioning wait function for rate limits

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var output = {
     "puuid" : "",
     "matches": [],
     "duoMatches": [],
+    "duoPlacements": [],
     "duoWins": 0,
     "duoLosses": 0,
     "duoLPChange": 0,
@@ -19,6 +20,7 @@ var output = {
     "puuid" : "",
     "matches": [],
     "duoMatches": [],
+    "duoPlacements": [],
     "duoWins": 0,
     "duoLosses": 0,
     "duoLPChange": 0,
@@ -41,7 +43,7 @@ async function fetchData(requestInput, typeOfRequest = false, username) {
     else if (typeOfRequest === "matchList"){
       var sectionOfMatches = 0
 
-      while (sectionOfMatches <= 2000){
+      while (sectionOfMatches <= 1000){
       var matchRequestURL = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + requestInput + "/ids?start=" + sectionOfMatches + "&count=100&api_key=" + apiKey;
       
 
@@ -57,22 +59,79 @@ async function fetchData(requestInput, typeOfRequest = false, username) {
       //starts comparison once matches have been pulled for both users
       if (Object.keys(output[userName1]["matches"]).length >= 1000 && Object.keys(output[userName2]["matches"]).length >= 1000) {
         const intersection = output[userName1]["matches"].filter(element => output[userName2]["matches"].includes(element));
-        console.log(intersection)
-
-        const fs = require('fs');
-        fs.writeFile("testing", JSON.stringify(output), 'utf8', function (err) {
-            if (err) {
-                return console.log(err);
-            }
         
-            console.log("The file was saved!");
-          }); 
+        //for (let i = 0; i < Object.keys(intersection).length; i++) THIS IS RIGHT CODE, SWAPPING SO WE DONT GET RATE LIMITED every 2 seconds
+        for (let i = 0; i < Object.keys(intersection).length; i++) {
+          fetchData(intersection[i], "matchInfo")
+        }
       }
-  
+    }
+    //for grabbing match data
+    else if (typeOfRequest === "matchInfo") {
+      let matchRequestURL = "https://americas.api.riotgames.com/tft/match/v1/matches/"+ requestInput + "?api_key=" + apiKey
+      var response = await fetch(matchRequestURL)
+      var data = await response.json();
 
-      }
+      //to catch failed requests because of rate limiting
+      data = rateLimitWait(data, matchRequestURL)
+      console.log(data)
+
+      const puuid1 = output[userName1]["puuid"]
+      const puuid2 = output[userName2]["puuid"]
+      console.log(requestInput)
+
+    //TO DO add function that sorts out games from different sets HERE
+
+    //gets which index each player is and then puts their placement into the dataset
+      let playerArray = []
+      playerArray = playerArray.concat(data["metadata"]["participants"])
+
+      let indexPlayer1 = playerArray.indexOf(puuid1)
+      let indexPlayer2 = playerArray.indexOf(puuid2)
+
+    //adds both player's placements to the output object
+      output[userName1]["duoPlacements"] = output[userName1]["duoPlacements"].concat(data["info"]["participants"][indexPlayer1]['placement'])
+      output[userName2]["duoPlacements"] = output[userName2]["duoPlacements"].concat(data["info"]["participants"][indexPlayer2]['placement'])
+
+    }
   }
 
+//slow down requests when rate limit is hit
+function rateLimitWait (dataReturned, URL) {
+
+  if (dataReturned["metadata"] == undefined) {
+    //short time out of two seconds to wait out (20 requests every 1 second) limit
+    setTimeout( () => {},2000);
+    let failedRequest = dataReturned
+    var response = await fetch(URL)
+    failedRequest = await response.json()
+
+    //long time out of two and a half minutes to wait out (100 requests every 2 minutes) limit
+    if (failedRequest["metadata"] == undefined){
+      setTimeout( () => {},150000);
+      var response = await fetch(URL)
+      failedRequest = await response.json()
+
+      if (failedRequest["metadata"] == undefined){
+        console.log("Request error. Try again later.")
+      }
+      else{
+        console.log("All good here after long wait!")
+        return failedRequest
+      }
+    }
+    else {
+      console.log("All good here after short wait!")
+      return failedRequest
+    }
+    
+
+  }
+  else {
+    console.log("All good here!")
+    return dataReturned
+  }
+}
 
 
 
