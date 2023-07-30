0351956bb6d4ce7baf8dab549774b5ae092770ea
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-07-30 17:57:53 -0400

    ryze and portal objects created

---
+++ my-app/src/riotAssets/portals.js
@@ -0,0 +1,133 @@
+const portals = {
+    Bandle_Cafeteria: {
+        region: "Bandle City",
+        info: "Gain a Spatula on 2-1. At shopping start, units equipped with a Spatula, emblem, or Tactician's Crown feed a Poro Snax to their closest ally, granting them 7 permanent health. Units will feed one Snax per item they have equipped.",
+    },
+    Scuttle_Puddle: {
+        region: "Bandle City",
+        info: "PvE Monsters are replaced by scuttle crabs that grant extra loot.",
+    },
+    Yordle_Portals: {
+        region: "Bandle City",
+        info: "On the 1st and 5th round each stage, get a free high-tier unit. The unit's tier increases with game time.",
+    },
+    Yuumis_Zoom_Zone: {
+        region: "Bandle City",
+        info: "When you buy XP, gain an additional 2.",
+    },
+    House_Lightshield: {
+        region: "Demacia",
+        info: "Gain a powerful Crown of Demacia on 2-1. If the wearer dies, you instantly lose the fight.",
+    },
+    Petricite_Forest: {
+        region: "",
+        info: "Start the game with 3 component anvils.",
+    },
+    Hearth_Home: {
+        region: "",
+        info: "On 4-5, gain a consumable Masterwork Upgrade that lets you upgrade one equipped item into its radiant version.",
+    },
+    Ornns_Forge: {
+        region: "",
+        info: "Randomly on either 2-5 or 3-5, gain an ornn anvil.",
+    },
+    God_Willows_Grove: {
+        region: "Ionia",
+        info: "A spot on your bench becomes the grove. The unit in it contributes their traits as though they were in your army.",
+    },
+    Placidium_Library: {
+        region: "",
+        info: "Gain a consumable on 3-5 that when you place it on a unit opens an armory with an emblem of each of their traits.",
+    },
+    The_Dreaming_Pool: {
+        region: "",
+        info: "At the start of each stage, gain a champ that really fits your team!",
+    },
+    Immortal_Bastion: {
+        region: "Noxus",
+        info: "On 2-1, gain a placeable battle flag that grants 20% attack speed and a 10% max health shield to units that start combat adjacent to it.",
+    },
+    Noxkraya: {
+        region: "Noxus",
+        info: "Each time you take an augment, a random arena hex appears. Units on an arena hex gain an item that they can use.",
+    },
+    Jayces_Workshop: {
+        region: "Piltover",
+        info: "All augments offered this game will be Prismatic.",
+    },
+    Stillwater_Hold: {
+        region: "Piltover",
+        info: "No augments will be offered this game.",
+    },  
+    The_University: {
+        name: "The University",
+        region: "",
+        info: "The first augment round will be a prismatic augment.",
+    },
+    Threshs_Sanctum: {
+        region: "",
+        info: "When ANY champion dies, collect their soul. Every 40 souls you collect, gain loot!",
+    },
+    Yoricks_Graveyard: {
+        name: "Yorick's Graveyard",
+        region: "",
+        info: "When ANY champion dies, collect their soul. Every 40 souls you collect, gain loot!",
+    },
+    Shifting_Sands: {
+        name: "Shifting Sands",
+        region: "",
+        info: "Every time a player dies, all players open an armory containing 4 of the eliminated players completed items.",
+    },
+    Shuriman_Bazaar: {
+        region: "",
+        info: "Component carousels have 2 components on them instead of 1.",
+    },
+    Warlords_Palace: {
+        name: "Warlords's Palace",
+        region: "Shurima",
+        info: "The 4-7 PvP round is replaced by an armory of treasure that can be rerolled infinitely for 2 gold per reroll.",
+    },
+    Marus_Omegnum: {
+        name: "Marus Omegnum",
+        region: "",
+        info: "",
+    },
+    Targon_Prime: {
+        region: "",
+        info: "",
+    },    
+    The_Summit: {
+        region: "",
+        info: "",
+    },
+    The_Lavender_Sea: {
+        name: "The Lavender Sea",
+        region: "Void",
+        info: "Starting on 2-1, after every carousel and PvE round, a void symbiote infects a new board Hex. The Hex grants 20% damage reduction if it's in the first two rows or 20% increased damage if it's in the back two rows.",
+    },
+    The_Rupture: {
+        name: "The Rupture",
+        region: "Void",
+        info: "Travel to a random location that is not currently shown.",
+    },
+    Unstable_Rift: {
+        name: "Unstable Rift",
+        region: "Void",
+        info: "Gain an unstable completed item that pops off and transforms into a random item every round.",
+    },
+    Ecliptic_Vaults: {
+        name: "Ecliptic Vaults",
+        region: "Zaun",
+        info: "Every time you select an augment, gain 4 gold per augment you have.",
+    },
+    Glasc_Industries: {
+        name: "Glasc Industries",
+        region: "",
+        info: "Every time you build a completed item, gain 3 gold.",
+    },
+    The_Sump: {
+        name: "The Sump",
+        region: "Zaun",
+        info: "There is no Interest this game. Instead, starting on 2-1, gain 3 gold every round.",
+    },
+}
