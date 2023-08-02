594f33969cbe7733bb6074fed50d25a7a0c12ec9
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-08-02 14:32:23 -0400

    added view toggle for components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,32 +8,95 @@ import TomeDisplay from './visuals/static/tomeDisplay';
 import PortalDisplay from './visuals/static/portalDisplay';
 import StarBasedWR from './visuals/starbasedWR';
 
-
-
 export function App() {
   const [dataPackage, setdataPackage] = useState(0);
-  const config = {headers: {'Content-Type': 'application/json'}}  
-  async function getData() {await axios.post('http://localhost:3002/api/data', config).then(response => setdataPackage(response['data']))}
+  const [showAugmentData, setShowAugmentData] = useState(true);
+  const [showTomeDisplay, setShowTomeDisplay] = useState(true);
+  const [showPortalDisplay, setShowPortalDisplay] = useState(true);
+  const [showUnitStats, setShowUnitStats] = useState(true);
+  const [showStarBasedWR, setShowStarBasedWR] = useState(true);
 
-  
-  
+  const config = { headers: { "Content-Type": "application/json" } };
+
+  async function getData() {
+    await axios
+      .post("http://localhost:3002/api/data", config)
+      .then((response) => setdataPackage(response["data"]));
+  }
+
   if (dataPackage === 0) {
-    getData()
+    getData();
+    return <div>wait one moment!</div>;
+  } else {
     return (
-      <div>wait one moment!</div>
-    )}
-  else{
-    return (
-      <div id='bigBox'>
-        <AugmentData/>
-        <TomeDisplay/>
-        <PortalDisplay/>
-        <UnitStats aboveUnitData={dataPackage[0]}/>
-        <StarBasedWR aboveUnitData={dataPackage[1]}/>
+      <div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showAugmentData}
+              onChange={() => setShowAugmentData(!showAugmentData)}
+            />
+            Show Augment Data
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showTomeDisplay}
+              onChange={() => setShowTomeDisplay(!showTomeDisplay)}
+            />
+            Show Tome Display
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showPortalDisplay}
+              onChange={() => setShowPortalDisplay(!showPortalDisplay)}
+            />
+            Show Portal Display
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showUnitStats}
+              onChange={() => setShowUnitStats(!showUnitStats)}
+            />
+            Show Unit Stats
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showStarBasedWR}
+              onChange={() => setShowStarBasedWR(!showStarBasedWR)}
+            />
+            Show Star Based WR
+          </label>
+        </div>
+        <div id="bigBox">
+        {/* Conditional rendering based on the state variables */}
+        {showAugmentData && <AugmentData />}
+        {showTomeDisplay && <TomeDisplay />}
+        {showPortalDisplay && <PortalDisplay />}
+        {showUnitStats && <UnitStats aboveUnitData={dataPackage[0]} />}
+        {showStarBasedWR && <StarBasedWR aboveUnitData={dataPackage[1]} />}
+        </div>
       </div>
-    )
+    );
   }
 }
+
   
   
 
