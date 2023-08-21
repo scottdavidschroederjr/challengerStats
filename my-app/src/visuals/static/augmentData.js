9d862633045e6a6b6c299f2b0f5a88c1978f7b7e
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-08-21 14:45:02 -0400

    standardized title box

--- my-app/src/visuals/static/augmentData.js
+++ my-app/src/visuals/static/augmentData.js
@@ -1,6 +1,7 @@
 function AugmentData() {
     return (
         <div className="component"id='augmentStatsSite'>
+            <div className="componentHeader">Augment Stats</div>
             <a href="https://tft-augment-stats.web.app/" target="_blank">Augment Stats Site</a>
         </div>
     )
--- my-app/src/visuals/static/tomeDisplay.js
+++ my-app/src/visuals/static/tomeDisplay.js
@@ -6,7 +6,10 @@ function TomeDisplay(){
     
     return (
         <div id='tomeBox' className='component'>
+            <div className='componentHeader'>Tome of Traits</div>
+            <div>
             <a href="https://www.metatft.com/tome-of-traits" target="_blank">Tome of Traits</a>
+            </div>
         </div>
     )
 }
