8a72712493566b64953d7cedfce1de3bc01cf6ef
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-08-31 19:14:49 -0400

    cashout setup, need to fill in table

--- my-app/src/core/alwaysRunning/constantFunctions.js
+++ my-app/src/core/alwaysRunning/constantFunctions.js
@@ -1,13 +1,13 @@
 const {challengerScrape} = require("../../riotRequests/combos/challengerScrape.js")
 const {updateAnalysisTable} = require("../analysis/updateAnalysisTable.js")
-const latestPatch = 1692187200
+const latestPatch = 1693371600
 const tftSetNumber = "TFTSet9"
 
 async function autoScrap(){
     while (latestPatch > 0) {
-        await updateAnalysisTable(tftSetNumber, latestPatch)
         await challengerScrape(latestPatch)
         await challengerScrape(latestPatch, 'grandmaster')
+        await updateAnalysisTable(tftSetNumber, latestPatch)
         await new Promise(resolve => setTimeout(resolve, 900000))
         await new Promise(resolve => setTimeout(resolve, 900000))
         await new Promise(resolve => setTimeout(resolve, 900000))
--- my-app/src/visuals/static/cashout.js
+++ my-app/src/visuals/static/cashout.js
@@ -1,10 +1,47 @@
+import React, { useState } from 'react';
+
+const cashoutTables = {
+    "0": <table>
+            <tr>
+                <th>Probability</th>
+                <th>Cashout</th>
+            </tr>
+            <tr>
+                <td>100%</td>
+                <td>1 Gold</td>
+            </tr>
+        </table>,
+
+};
+
 function Cashout() {
-    return (
-        <div className="component"id=''>
-            <div className="componentHeader">Piltover Cashout</div>
-            <a href="https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fs4hj0cenq5hb1.png" target="_blank">Cashout Table</a>
-        </div>
-    )
+    const [lossStreak, setlossStreak] = useState("none");
+
+        return (
+            <div className="component"id=''>
+                <div className="componentHeader">Piltover Cashout</div>
+
+                <div>
+                    <select onChange={(e) => setlossStreak(e.target.value)}>
+                    <option value="none">Cashout Size</option>
+                    <option value="0">0 Loss / 1 Energy</option>
+                    <option value="1">1 Loss / 2 Energy</option>
+                    <option value="2">2 Loss / 4 Energy</option>
+                    <option value="3">3 Loss / 6 Energy</option>
+                    <option value="4">4 Loss / 9 Energy</option>
+                    <option value="5">5 Loss / 12 Energy</option>
+                    <option value="6">6 Loss / 16 Energy</option>
+                    <option value="7">7 Loss / 20 Energy</option>
+                    <option value="8">8 Loss / 25 Energy</option>
+                    </select>
+                
+                <br></br>
+                {cashoutTables[lossStreak]}
+                </div>
+            </div>
+        )
+
 }
+
 
 export default Cashout
