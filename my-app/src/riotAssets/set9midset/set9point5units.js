0e524c358b0c4ced72e79754d6f2e3db3d4ed041
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-09-06 17:51:21 -0400

    added cost for 95

---
+++ my-app/src/riotAssets/set9midset/set9point5units.js
@@ -0,0 +1,13 @@
+const midset9units = {
+    "cost": {
+        1: ['Cassiopeia',  "Cho'Gath", "Graves", "Illaoi", 'Irelia', 'Jhin', 'Kayle', 'Malzahar', "Milio", 'Orianna', 'Poppy',  'Renekton', 'Samira'],
+        2: ['Ashe', "Galio", "Jinx", "Kassadin", "Naafiri", "Qiyana", "Sett", "Soraka", "Swain", "Taliyah", "Twisted Fate", "Vi", "Warwick"],
+        3: ["Darius", "Ekko", "Jayce", "Karma", "Katarina", "Miss Fortune", "Nautilus", "Neeko", "Quinn", "Rek'Sai", "Sona", "Taric", "Vel'Koz"],
+        4: ["Aphelios", "Azir", "Fiora", "Jarvan IV", "Kai'Sa", "Mordekaiser", "Nasus", "Nilah", "Sejuani", "Shen", "Slico", "Xayah"],
+        5: ['Ryze', "Gangplank", 'Aatrox', 'Heimerdinger', 'Ahri', 'Sion', "Bel'Veth", "K'Sante"],
+    },
+    "traits": {
+
+    },
+
+}
