881a9dce6e449bc756e2831d51282e17665bf6c1
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-09-07 15:50:06 -0400

    Added data for support items

--- my-app/src/riotAssets/set9midset/set9midItems.js
+++ my-app/src/riotAssets/set9midset/set9midItems.js
@@ -1,7 +1,113 @@
 const itemList = {
-    "TFT4_Item_OrnnObsidianCleaver": {
+//Support Items
+    "": {
+        "name": "Aegis of the Legion",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+
+    },
+    "": {
+        "name": "Banshee's Veil",
+        "type": "Support",
+        "icon": "",
+    },
+    "": {
+        "name": "Chalice of Power",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Crest of Cinders",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Locket of the Iron Solari",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Needlessly Big Gem",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Obsidian Cleaver",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Randuin's Omen",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Shroud of Stillness",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Virtue of the Martyr",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Zeke's Herald",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Zephyr",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
+        "name": "Zz'Rot Portal",
+        "type": "Support",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+    "": {
         "name": "",
-    }
+        "type": "",
+        "icon": "",
+        "info": "",
+        "comps": "",
+    },
+//Normal Items
+
+//Trait Items
+
+//Artifact Items
+
+//Radiant Items
+
+//Zaun Items
+
+//Turret Ittems
 }
 
 module.exports = itemList
