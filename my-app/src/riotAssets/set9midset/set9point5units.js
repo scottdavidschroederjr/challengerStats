8615e3831b636a4efb143879a0f8352ac1775db7
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-09-07 12:27:17 -0400

    filled out midset unit info

--- my-app/src/riotAssets/set9midset/set9point5units.js
+++ my-app/src/riotAssets/set9midset/set9point5units.js
@@ -7,7 +7,30 @@ const midset9units = {
         5: ['Ryze', "Gangplank", 'Aatrox', 'Heimerdinger', 'Ahri', 'Sion', "Bel'Veth", "K'Sante"],
     },
     "traits": {
-
+        "Bilgewater": ["Graves", "Illaoi", "Twisted Fate", "Miss Fortune", "Nautilus", "Nilah", "Gankplank"],
+        "Darkin": ["Naafiri", "Aatrox"],
+        "Demacia": ["Kayle", "Poppy", "Galio", "Quinn", "Sona", "Jarvan IV"],
+        "Freljord": ["Ashe", "Sejuani"],
+        "Ionia": ['Irelia', 'Jhin', 'Sett', 'Xayah', 'Karma', 'Shen','Ahri'],
+        "Ixtal": ['Milio', "Qiyana", "Neeko"],
+        "Noxus": ['Cassiopeia', 'Samira', 'Swain', 'Darius', 'Katarina', 'Mordekaiser', 'Sion'],
+        "Piltover": ['Orianna', 'Vi','Ekko', 'Jayce', 'Heimerdinger'],
+        "Shurima": ['Cassiopeia', 'Renekton', 'Taliyah', 'Naafiri', 'Azir', 'Nasus', "K'Sante"],
+        "Targon": ['Soraka', 'Taric', 'Aphelios'],
+        "Void": ["Cho'Gath", 'Malzahar', 'Kassadin', 'Reksai', 'Velkoz', "Kai'Sa", 'Belveth'],
+        "Zaun": ['Jinx', 'Warwick', 'Ekko', 'Urgot', 'Silco'],
+        "Bastion": ['Illaoi', 'Poppy', 'Kassadin', 'Neeko', 'Taric', 'Shen', "K'Sante"],
+        "Bruiser": ["Cho'Gath", 'Renekton', 'Vi', "Rek'Sai", 'Sejuani', 'Sion'],
+        "Challenger": ['Irelia', 'Samira', 'Warwick', 'Naafiri', "Kai'Sa", 'Fiora'],
+        "Gunner": ['Graves', 'Jinx', 'Jayce', "Aphelios", "Gangplank"],
+        "Invoker": ['Cassiopeia', 'Milio', 'Galio', 'Soraka', 'Karma', 'Ryze', 'Shen'],
+        "Juggernaut": ['Sett','Warwick', 'Darius', 'Nautilus', 'Nasus', 'Aatrox'],
+        "Multicaster": ['Taliyah', "Twisted Fate", 'Sona', "Vel'Koz"],
+        "Rogue": ['Graves', 'Qiyana', 'Ekko', 'Katarina'],
+        "Slayer": ['Kayle', 'Qiyana', 'Quinn', "Rek'Sai", "Mordekaiser", 'Aatrox'],
+        "Sorcerer": ['Malzahar', 'Orianna', 'Swain', 'Taric', 'Velkoz', 'Silco', 'Ahri'],
+        "Strategist": ['Swain', "Miss Fortune", 'Azir', 'Jarvan IV'],
+        "Vanquisher": ['Jhin', "Ashe", "Darius", "Nilah", "Xayah"]
     },
 
 }
