bb424707fd9e89cb633ff0d742b1b75c3a2fe86b
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-09-09 13:14:45 -0400

    Ryze data now displays!

--- my-app/src/visuals/static/portalDisplay.js
+++ my-app/src/visuals/static/portalDisplay.js
@@ -1,20 +1,68 @@
 import React, { useState } from 'react';
-const ryzeInfo = require("../../riotAssets/ryze.js")
 
-function PortalDisplay({aboveRyzeData}){
+function PortalDisplay({ aboveRyzeData }) {
+  const [sortBy, setSortBy] = useState(null);
+  const [sortOrder, setSortOrder] = useState('asc');
+
+  // Function to toggle sorting order
+  const toggleSortOrder = () => {
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+  };
+
+  const handleSort = (column) => {
+    if (sortBy === column) {
+      toggleSortOrder();
+    } else {
+      setSortBy(column);
+      setSortOrder('asc');
+    }
+  };
+
+  // Sort the data based on the selected column and order
+  const sortedData = [...aboveRyzeData].sort((a, b) => {
+    if (sortBy === 'Count' || sortBy === 'Place') {
+      // Convert the values to numbers for numeric columns
+      const valueA = parseInt(a[sortBy] || 0, 10);
+      const valueB = parseInt(b[sortBy] || 0, 10);
+
+      return sortOrder === 'asc' ? valueA - valueB : valueB - valueA;
+    } else {
+      const valueA = (a[sortBy] || '').toUpperCase(); // Handle undefined values
+      const valueB = (b[sortBy] || '').toUpperCase(); // Handle undefined values
+
+      if (valueA < valueB) return sortOrder === 'asc' ? -1 : 1;
+      if (valueA > valueB) return sortOrder === 'asc' ? 1 : -1;
+      return 0;
+    }
+  });
 
   return (
     <div className="component">
-        <div className="componentHeader">Ryze Info</div>
-
+      <div className="componentHeader">Ryze Info</div>
+      <div id='ryzeTable'>
+        <table>
+          <thead>
+            <tr>
+              <th onClick={() => handleSort('Region')}>Region</th>
+              <th onClick={() => handleSort('Count')}>Count</th>
+              <th onClick={() => handleSort('Place')}>Place</th>
+              <th onClick={() => handleSort('Spell')}>Spell</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedData.map((data, index) => (
+              <tr key={index}>
+                <td>{data[3]}</td>
+                <td>{data[1]}</td>
+                <td>{data[2]}</td>
+                <td>{data[4]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-)
-};
+  );
+}
 
 export default PortalDisplay;
-
-
-
-
-
-
