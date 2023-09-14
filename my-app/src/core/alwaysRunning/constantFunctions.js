359e03d8681b47ca05c28ddb1fb2dce73d2d2a9f
Scott Schroeder <scottdavidschroederjr@gmail.com>
2023-09-13 20:02:53 -0400

    adjusted epoch for set 9.5 launch

--- my-app/src/core/alwaysRunning/constantFunctions.js
+++ my-app/src/core/alwaysRunning/constantFunctions.js
@@ -1,13 +1,15 @@
 const {challengerScrape} = require("../../riotRequests/combos/challengerScrape.js")
 const {updateAnalysisTable} = require("../analysis/updateAnalysisTable.js")
-const latestPatch = 1694189343
+const latestPatch = 1694610000
 const tftSetNumber = "TFTSet9"
 
 async function autoScrap(){
     while (latestPatch > 0) {
         await updateAnalysisTable(tftSetNumber, latestPatch)
         await challengerScrape(latestPatch)
-        //await challengerScrape(latestPatch, 'grandmaster')
+        await challengerScrape(latestPatch, 'grandmaster')
+        await challengerScrape(latestPatch, 'master')
+
         
         
         await new Promise(resolve => setTimeout(resolve, 900000))
